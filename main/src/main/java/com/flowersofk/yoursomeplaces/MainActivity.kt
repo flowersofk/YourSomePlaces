c64d34ff955d17808a1a49ea9d0d3d3594e53223
JenifferPark <sakuraflow@naver.com>
2021-01-27 23:45:15 +0900

    develop_commit_1

--- main/src/main/java/com/flowersofk/yoursomeplaces/MainActivity.kt
+++ main/src/main/java/com/flowersofk/yoursomeplaces/MainActivity.kt
@@ -19,7 +19,7 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var placeViewModel: PlaceViewModel
     
-    private var test = "user_3_service_3_commit_1"
+    private var test = "develop_commit_1"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
