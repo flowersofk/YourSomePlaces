09c5b17d13b6cb64dc2179c2771d3cdb517b0fd5
박민석 <flowersofk@gmail.com>
2021-01-27 16:56:03 +0900

    branch_commit_push_1

--- main/src/main/java/com/flowersofk/yoursomeplaces/MainActivity.kt
+++ main/src/main/java/com/flowersofk/yoursomeplaces/MainActivity.kt
@@ -19,7 +19,7 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var placeViewModel: PlaceViewModel
     
-    private var sourcetree_pr_test = "commit_4"
+    private var sourcetree_pr_test = "commit_service_1"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
