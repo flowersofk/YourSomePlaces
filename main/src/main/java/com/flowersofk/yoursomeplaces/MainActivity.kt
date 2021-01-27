49d3f1591acbbfd3d8125378b6bd5f8b480a7311
sakuraflow <sakuraflow@naver.com>
2021-01-27 17:29:50 +0900

    user_1_commit_1

--- main/src/main/java/com/flowersofk/yoursomeplaces/MainActivity.kt
+++ main/src/main/java/com/flowersofk/yoursomeplaces/MainActivity.kt
@@ -19,7 +19,7 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var placeViewModel: PlaceViewModel
     
-    private var test = "service_2_commit_3"
+    private var test = "user_1_commit_1"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
